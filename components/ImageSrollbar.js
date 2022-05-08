ea090524d6373e1e8502d7fcf8ef04c37e169b15
Amdebirhan <amdebirhanteshome@gmail.com>
2022-05-08 03:46:01 -0700

    done

--- components/ImageSrollbar.js
+++ components/ImageSrollbar.js
@@ -14,7 +14,7 @@ const LeftArrow = () => {
         onClick={() => scrollPrev()}
         fontSize='2xl'
         cursor='pointer'
-        d={['none','none','none','block']}
+        d={['none', 'none', 'none', 'block']}
       />
     </Flex>
   );
@@ -30,17 +30,17 @@ const RightArrow = () => {
         onClick={() => scrollNext()}
         fontSize='2xl'
         cursor='pointer'
-        d={['none','none','none','block']}
-    />
+        d={['none', 'none', 'none', 'block']}
+      />
     </Flex>
   );
 }
 export default function ImageSrollbar({ data }) {
   return (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{ overflow: 'hidden' }} >
-      {data.map((item) => (
-        <Box width='910px' itemId={item.id} overflow='hidden' p='1'>
-          <Image placeholder="blur" blurDataURL={item.url} src={item.url} width={1000} height={500}  sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px" />
+      {data.map((item, index) => (
+        <Box width='910px' itemId={item.id} overflow='hidden' p='1' key={index} >
+          <Image placeholder="blur" blurDataURL={item.url} src={item.url} width={1000} height={500} sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px" />
         </Box>
       ))}
     </ScrollMenu>
